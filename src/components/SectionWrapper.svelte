bc285edf1da37f08feef4a2cfb0b629833094b52
UnwntdEngr <158253674+UnwntdEngr@users.noreply.github.com>
2024-03-26 12:03:47 +0800

    Add files via upload

---
+++ src/components/SectionWrapper.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let id;
+</script>
+<section {id} class="min-h-screen flex flex-c px-4">
+    <div class="flex flex-col flex-a max-w-[1400px] mx-auto w-full">
+        <slot />
+    </div>
+</section>
